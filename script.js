ad91c4fdf2711f4be9a18aa956413a2aa14ee7a7
Samuel <samuelldmj5@gmail.com>
2024-02-01 17:37:38 +0100

    javascript learning

--- script.js
+++ script.js
@@ -1,2 +1,91 @@
+//the console was built to execute small pieces of code
+//we can run javascript on a page by linking it to the HTML file with the script tag
+//if we want to output something into the browser console, we use console.log()
+//we dont need to use console.log when on the browser console, hitting enter will execute it
+//to move to the next line use shift + enter
 
-console.log(40+20-30)
+//VALUES
+// console.log('Bolu')
+// console.log(23)
+
+//VARIABLES
+//camelCase: firstName
+//snake_case: first_name
+//convention does not support the use of uppercase for variables
+//uppercase letter are specific for CONSTANT
+let fname = 'Samuel';
+let $number = 20;
+let dec = 2.777;
+let javascriptisFun = true
+// console.log(fname)
+// console.log($number)
+//make your variable name descriptive
+let formerJob = 'Programmer';
+let currentJob = 'Teacher';
+// console.log(formerJob)
+// console.log(currentJob)
+
+
+//DATA TYPES
+//value is either an object or primitive 
+//there are 7 primitive data types (strings, boolean, number, null, big int, undefined and symbol )   
+//Number: this include integers and decimals
+//String: text value, put them in quotes
+//boolean: True, False
+//Undefined: empty value
+//null: empty value
+//symbol: unique value that can not be changed
+//Bigint: value that can not changed
+
+//COMMENTS
+// Single line comment: //
+//Multi line comments: /**/
+
+//typeof
+//it is an operator that can be used to show the types of a values
+// console.log(typeof currentJob);
+// console.log(typeof $number);
+// console.log(typeof javascriptisFun);
+// console.log(typeof dec);
+
+//DYNAMIC TYPING
+javascriptisFun = "Yes!";
+//console.log(typeof javascriptisFun)
+
+//LET, VAR and CONST
+let age = 30;
+//reassigning age
+age = 31
+
+//const is an immutable keyword, you can't change it
+const birthYear = 1992;
+//console.log(birthYear)
+
+//OPERATORS (*, -, /, +, **)
+//COMPARISON OPERATORS(<, >, >=, <=)
+//OPERATOR PRECEDENCE -> consult MDN precedence operator
+const now = 2037
+const ageSam =  now - 1991
+const ageDan = now - 2018
+// console.log(now - ageSam > now - ageDan)
+// console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
