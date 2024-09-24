df84cee1c3f0ee25dc142f30818aab04323a8de2
Samuel <samuelldmj5@gmail.com>
2024-09-24 14:32:06 +0100

    challenge 16. solution moved

--- challenges/challenge16.js
+++ challenges/challenge16.js
@@ -12,13 +12,3 @@ Your tasks:
 */
 
 
-(function () {
-    const header = document.querySelector('h1');
-    header.style.color = 'tomato';
-    let bdy = document.querySelector('body');
-
-    bdy.addEventListener('click', function () {
-        header.style.color = 'blue';
-
-    })
-})();
