4fa5aea723133a3c8bd72958ddac1391469d93b8
samuelldmj <samuelldmj5@gmail.com>
2024-10-31 15:18:39 +0100

    first part completed

--- challenges/challenge3.js
+++ challenges/challenge3.js
@@ -1,5 +1,6 @@
 /*Coding Challenge #3 |  JavaScript 🔥
-There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+There are two gymnastics teams, Dolphins and Koalas.
+ They compete against each other 3 times. The winner with the highest average score wins a trophy!
 
 Your tasks:
 1. Calculate the average score for each team, using the test data below
@@ -20,3 +21,40 @@ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
 Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 */
 
+
+//solution
+const dolphinScores = [96, 108, 89];
+const kaolaScores = [88, 91, 110];
+
+let dolphinAvg = dolphinScores.reduce((pre, cur) => pre + cur) / dolphinScores.length;
+// console.log(dolphinAvg.toFixed(1)); 
+
+let kaolaAvg = kaolaScores.reduce((pre, cur) => pre + cur) / kaolaScores.length;
+// console.log(kaolaAvg.toFixed(1)); 
+
+if(dolphinAvg > kaolaAvg){
+    // console.log('Dolphin is the winner')
+}else if (kaolaAvg > dolphinAvg){
+    // console.log('Kaola is the winner!');
+}else {
+    // console.log('It is a draw')
+}
+
+let minimumScore = 100; 
+if((dolphinAvg >= minimumScore) && (dolphinAvg > kaolaAvg)){
+    // console.log('Dolphin is the winner')
+}else if((kaolaAvg >= minimumScore) && (kaolaAvg > dolphinAvg)){
+    // console.log('Kaola is th winner')
+}
+
+/*
+Minimum score also applies to a draw! So a draw only
+happens when both teams have the same score and both have a score greater or equal 100 points.
+Otherwise, no team wins the trophy
+*/
+
+if((dolphinAvg >= minimumScore) && (kaolaAvg >= minimumScore) && (kaolaAvg === dolphinAvg)){
+    console.log('it is a draw!')
+}else {
+    console.log('No team wins the trophy')
+}
--- oopimplementation/script.js
+++ oopimplementation/script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -11,11 +10,100 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-console.log(form.classList)
+// console.log(form.classList)
 
-let map, mapEvent;
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+// let map, mapEvent;
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//         console.log(position);
+//         let latitude = position.coords.latitude;
+//         let longitude = position.coords.longitude;
+//         // console.log(latitude, longitude);
+//         console.log(`https://www.google.com/maps/dir/@${latitude},${longitude}`);
+
+//         let coords = [latitude, longitude];
+//         map = L.map('map').setView(coords, 11);
+
+//         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         }).addTo(map);
+
+//         // L.marker(coords).addTo(map)
+//         //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//         //     .openPopup();
+
+//             //handling clicks on map
+//             map.on('click', function(mapE){
+//                 mapEvent = mapE;
+//                 form.classList.remove('hidden');
+//                 inputDistance.focus();
+//              })
+//     }, function () {
+//         alert('Could not get your position')
+//     })
+// }
+
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     inputCadence.value = inputDistance.value = inputDuration.value = inputDuration.value = '';
+//     console.log(mapEvent);
+//     const { lat, lng } = mapEvent.latlng;
+
+//     L.marker([lat, lng]).addTo(map)
+//         .bindPopup(L.popup({
+//             maxWidth: 250,
+//             minWidth: 100,
+//             autoClose: false,
+//             closeOnClick: false,
+//             className: 'running-popup'
+//         })).setPopupContent('Workout')
+//         .openPopup();
+// })
+
+
+
+// inputType.addEventListener('change', function () {
+//     /*
+//     In JavaScript, the classList property provides a convenient way to manipulate the class attribute of an HTML element.
+//     It allows you to add, remove, toggle, and check for the presence of specific classes.
+//     */
+//     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+//     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+// })
+
+// console.dir(navigator);
+
+/*
+REFACTORING CODE: USINNG OOP PATTERN
+*/
+class App {
+    //these are private instance properties. 
+    #map;
+    #mapEvent;
+
+ /*
+Constructor: The Constructor get called immediately when an object is created from the class.
+*/
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+
+    _getPosition() {
+        if (navigator.geolocation) {
+            //before alteration this._loadMap is treated as ordinary function not method
+            //in a regular function call, the "this" keyword is set to undefined.
+            // you need to bind the function to the current object, hence we change from this._loadMap to this._loadMap.bind(this)
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert('Could not get your position')
+            })
+        }
+
+    }
+
+    _loadMap(position) {
         console.log(position);
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
@@ -23,55 +111,53 @@ if (navigator.geolocation) {
         console.log(`https://www.google.com/maps/dir/@${latitude},${longitude}`);
 
         let coords = [latitude, longitude];
-        map = L.map('map').setView(coords, 11);
-
+        this.#map = L.map('map').setView(coords, 11);
+        // console.dir(L);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
 
         // L.marker(coords).addTo(map)
         //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         //     .openPopup();
 
-            //handling clicks on map
-            map.on('click', function(mapE){
-                mapEvent = mapE;
-                form.classList.remove('hidden');
-                inputDistance.focus();
-            
-            
-             })
-    }, function () {
-        alert('Could not get your position')
-    })
+        //handling clicks on map
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        /*
+        In JavaScript, the classList property provides a convenient way to manipulate the class attribute of an HTML element.
+        It allows you to add, remove, toggle, and check for the presence of specific classes.
+        */
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+        inputCadence.value = inputDistance.value = inputDuration.value = inputDuration.value = '';
+        // console.log(mapEvent);
+        const { lat, lng } = this.#mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            })).setPopupContent('Workout')
+            .openPopup();
+    }
 }
 
-form.addEventListener('submit', function(e){
-        e.preventDefault();
 
-        inputCadence.value = inputDistance.value = inputDuration.value = inputDuration.value = '';
-        console.log(mapEvent);
-        const {lat, lng} = mapEvent.latlng;
-
-                L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth:100,
-                    autoClose:false,
-                    closeOnClick:false,
-                    className:'running-popup'
-                })).setPopupContent('Workout')
-                .openPopup();
-
-})
-
-inputType.addEventListener('change', function(){
-    /*
-    In JavaScript, the classList property provides a convenient way to manipulate the class attribute of an HTML element.
-    It allows you to add, remove, toggle, and check for the presence of specific classes.
-    */
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
-
-// console.dir(navigator);
+//instance or object of class App
+const app = new App();
