8c6735f5b3194d6f86b6a06da5a2743fe411f4cf
samuelldmj <samuelldmj5@gmail.com>
2024-11-01 18:29:21 +0100

    refactoring and adding of cycling, running and Workoutclass

--- oopimplementation/script.js
+++ oopimplementation/script.js
@@ -77,10 +77,58 @@ const inputElevation = document.querySelector('.form__input--elevation');
 /*
 REFACTORING CODE: USINNG OOP PATTERN
 */
+
+class Workout{
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+
+    constructor(coords, distance, duration){
+        this.coords = coords; //[lat, lng]
+        this.duration = duration; //in min
+        this.distance = distance; //in km
+    }
+}
+
+
+class Running extends Workout {
+    type = 'running';
+
+    constructor(coords, distance, duration, cadence){
+        super(coords, distance, duration)
+            this.cadence = cadence;
+            this.calcPace();
+    }
+   
+    calcPace(){
+        this.pace = this.duration / this.distance;
+        return this.pace
+    }
+}
+class Cycling extends Workout {
+    type = 'cycling';
+
+    constructor(coords, distance, duration, elevationGain){
+        super(coords, distance, duration)
+            this.elevationGain = elevationGain; 
+    }
+
+    calcSpeed(){
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+const run1 = new Running([7.4124, 3.345], 5.5, 24, 178);
+const cycling1 = new Cycling([7.4124, 3.345], 30, 13, 300);
+console.log(run1, cycling1);
+
+
+////////////////////////////////////////
 class App {
     //these are private instance properties. 
     #map;
     #mapEvent;
+    #workout = [];
 
  /*
 Constructor: The Constructor get called immediately when an object is created from the class.
@@ -126,6 +174,7 @@ Constructor: The Constructor get called immediately when an object is created fr
     }
 
     _showForm(mapE) {
+        console.log(mapE);
         this.#mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
@@ -142,19 +191,70 @@ Constructor: The Constructor get called immediately when an object is created fr
 
     _newWorkout(e) {
         e.preventDefault();
-        inputCadence.value = inputDistance.value = inputDuration.value = inputDuration.value = '';
-        // console.log(mapEvent);
-        const { lat, lng } = this.#mapEvent.latlng;
 
-        L.marker([lat, lng]).addTo(this.#map)
-            .bindPopup(L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup'
-            })).setPopupContent('Workout')
-            .openPopup();
+        //validating input
+        // let validInput = (...inputs) => (!Number.isFinite(inputs));
+        //using every
+        const validInput = (...inputs) => inputs.every( inp => Number.isFinite(inp));
+        const allPositive = (...inputs) => inputs.every( inp => inp > 0);
+
+        //Get the data from form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+        const { lat, lng } = this.#mapEvent.latlng;
+        let workout;
+
+        
+        //check if workout is running, create running object;
+        if(type === 'running'){
+            const cadence = +inputCadence.value
+            //check if data is valid.
+            if(!validInput(distance, duration, cadence) || !allPositive(distance, duration, cadence)){
+                return alert('Input have to be positive numbers');
+            }  
+
+            workout = new Running([lat, lng], distance, duration, cadence);
+            
+        }
+
+        //if workout cycling, create cycling object
+        if(type === 'cycling'){
+            const elevation = +inputElevation.value
+             //check if data is valid.
+             if(!validInput(distance, duration, elevation) || !allPositive(distance, duration)){
+                return alert('Input have to be positive numbers');
+             } 
+
+             workout = new Cycling([lat, lng], distance, duration, elevation);
+        }
+
+        //hide form and clear input fields.
+        inputCadence.value = inputDistance.value = inputDuration.value = inputDuration.value = '';
+        
+        //add new object to workout array
+        this.#workout.push(workout);
+        console.log(workout);
+
+        //render workout on map as marker
+       this._renderWorkerMarker(workout);
+    }
+
+    _renderWorkerMarker(workout){
+        L.marker(workout.coords).addTo(this.#map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: `${workout.type}-popup`
+        })).setPopupContent(`${workout.type}`)
+        .openPopup();
+    }
+
+    //render workout list
+    _renderWorkout(workout){
+        
     }
 }
 
